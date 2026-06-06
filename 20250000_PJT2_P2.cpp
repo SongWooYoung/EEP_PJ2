a4fc3ba0e01d1c666e39b67b2fd7a6ca90dc036c
WooYoung Song <swy0121@unist.ac.kr>
2026-06-06 10:43:04 +0900

    Matrix view

--- 20250000_PJT2_P2.cpp
+++ 20250000_PJT2_P2.cpp
@@ -5,14 +5,39 @@ using namespace std;
 
 const int MAX_N = 4;
 
-void PrintMatrix(int n, double matrix[MAX_N][MAX_N + 1]) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n + 1; j++) {
-            cout << matrix[i][j] << " ";
+
+struct MatrixView {
+    int n;
+    double (*matrix)[MAX_N + 1];
+
+    MatrixView& operator/=(double value) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                matrix[i][j] /= value;
+            }
         }
-        cout << endl;
+        return *this;
     }
-}
+
+    MatrixView& operator-=(const MatrixView& other) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                matrix[i][j] -= other.matrix[i][j];
+            }
+        }
+        return *this;
+    }
+
+    friend ostream& operator<<(ostream& os, const MatrixView& view) {
+        for (int i = 0; i < view.n; i++) {
+            for (int j = 0; j < view.n + 1; j++) {
+                os << view.matrix[i][j] << " ";
+            }
+            os << '\n';
+        }
+        return os;
+    }
+};
 
 void readAugmentedMatrix(ifstream& inputFile, int& n, double matrix[MAX_N][MAX_N + 1]) {
     // TODO: Fill this function
@@ -27,16 +52,27 @@ void readAugmentedMatrix(ifstream& inputFile, int& n, double matrix[MAX_N][MAX_N
     }
 
     // for debugging
-    PrintMatrix(n, matrix);
+    MatrixView view{n, matrix};
+    cout << view;
 
 }
 
 void normalizePivotRow(int n, double matrix[MAX_N][MAX_N + 1], int pivot) {
     // TODO: Fill this function
+    // 1. At pivot-th column, unify the pivot row 
+    // it is for sure that if prev step is done correctly,  
+    // values under pivot-th column is 0, except the pivot row.
+
+    double pivotVal = matrix[pivot][pivot];
+
+    MatrixView view{n, matrix};
+    view /= pivotVal;
+
 }
 
 void eliminateColumn(int n, double matrix[MAX_N][MAX_N + 1], int pivot) {
     // TODO: Fill this function
+
 }
 
 void reduceToIdentity(int n, double matrix[MAX_N][MAX_N + 1]) {
