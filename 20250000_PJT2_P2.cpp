10e7e229b4b8fa712de54af82354eba8840d9656
WooYoung Song <swy0121@unist.ac.kr>
2026-06-06 11:11:49 +0900

    Why,, The matrix are not modified..?

--- 20250000_PJT2_P2.cpp
+++ 20250000_PJT2_P2.cpp
@@ -5,27 +5,38 @@ using namespace std;
 
 const int MAX_N = 4;
 
+struct RowView {
+    double* row;
+    int width;
+
+    // Watch only row array
+
+    // Hadamard subtraction
+    RowView& operator-=(const RowView& right) {
+        for (int i = 0; i < width; i++) {
+            row[i] -= right.row[i];
+        }
+        return *this;
+    }
+
+    // scalar division
+    RowView& operator/=(const double& value) {
+        for (int i = 0; i < width; i++) {
+            row[i] /= value;
+        }
+        return *this;
+    }
+
+};
+
 
 struct MatrixView {
     int n;
     double (*matrix)[MAX_N + 1];
 
-    MatrixView& operator/=(double value) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n + 1; j++) {
-                matrix[i][j] /= value;
-            }
-        }
-        return *this;
-    }
-
-    MatrixView& operator-=(const MatrixView& other) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n + 1; j++) {
-                matrix[i][j] -= other.matrix[i][j];
-            }
-        }
-        return *this;
+    // Focus on row array
+    RowView operator[](int index) {
+        return RowView{matrix[index], n + 1};
     }
 
     friend ostream& operator<<(ostream& os, const MatrixView& view) {
@@ -52,8 +63,8 @@ void readAugmentedMatrix(ifstream& inputFile, int& n, double matrix[MAX_N][MAX_N
     }
 
     // for debugging
-    MatrixView view{n, matrix};
-    cout << view;
+    // MatrixView view{n, matrix};
+    // cout << view;
 
 }
 
@@ -66,21 +77,45 @@ void normalizePivotRow(int n, double matrix[MAX_N][MAX_N + 1], int pivot) {
     double pivotVal = matrix[pivot][pivot];
 
     MatrixView view{n, matrix};
-    view /= pivotVal;
+    // view[i] is temporary Object , which is not permiitted to be used as left of friend function
+    // Change scalar division operator to Member function to solve this problem
+    for (int i = pivot; i < n; i++) {
+        view[i] /= (pivotVal / matrix[i][pivot]);
+    }
 
 }
 
 void eliminateColumn(int n, double matrix[MAX_N][MAX_N + 1], int pivot) {
     // TODO: Fill this function
+    // 1. Eliminate the pivot-th column, except the pivot row.
+    MatrixView view{n, matrix};
+    for (int i = pivot+1; i < n; i++) {
+        view[i] -= view[pivot];
+    }
 
 }
 
 void reduceToIdentity(int n, double matrix[MAX_N][MAX_N + 1]) {
     // TODO: Fill this function
+    for (int pivot = 0; pivot < n; pivot++) {
+        normalizePivotRow(n, matrix, pivot);
+        eliminateColumn(n, matrix, pivot);
+
+        cout << "After pivot " << pivot << ":\n";
+        MatrixView view{n, matrix};
+        cout << view;
+    }
 }
+
 
 void printSolution(int n, double matrix[MAX_N][MAX_N + 1]) {
     // TODO: Fill this function
+    // Print Last column values as solution
+
+    for (int i = 0; i < n; i++) {
+        cout << "x[" << i << "] = " << matrix[i][n] << endl;
+    }
+
 }
 
 // -----------------------------------------------------------------------
@@ -104,8 +139,8 @@ int main() {
     }
 
     readAugmentedMatrix(inputFile, n, matrix);
-    // reduceToIdentity(n, matrix);
-    // printSolution(n, matrix);
+    reduceToIdentity(n, matrix);
+    printSolution(n, matrix);
 
     inputFile.close();
 
