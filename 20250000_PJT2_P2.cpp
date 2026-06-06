4d5d77b6ccc0d89b5569e01139e970b2b849b1a5
WooYoung Song <swy0121@unist.ac.kr>
2026-06-06 10:09:54 +0900

    Problem 2, readFile

--- 20250000_PJT2_P2.cpp
+++ 20250000_PJT2_P2.cpp
@@ -5,8 +5,30 @@ using namespace std;
 
 const int MAX_N = 4;
 
+void PrintMatrix(int n, double matrix[MAX_N][MAX_N + 1]) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n + 1; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
 void readAugmentedMatrix(ifstream& inputFile, int& n, double matrix[MAX_N][MAX_N + 1]) {
     // TODO: Fill this function
+    // In main function this must work as follows:
+    // 1. Read the size of the Matrix size N
+    inputFile >> n;
+    // 2. Copy the value to the input argument
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n + 1; j++) {
+            inputFile >> matrix[i][j];
+        }
+    }
+
+    // for debugging
+    PrintMatrix(n, matrix);
+
 }
 
 void normalizePivotRow(int n, double matrix[MAX_N][MAX_N + 1], int pivot) {
@@ -46,8 +68,8 @@ int main() {
     }
 
     readAugmentedMatrix(inputFile, n, matrix);
-    reduceToIdentity(n, matrix);
-    printSolution(n, matrix);
+    // reduceToIdentity(n, matrix);
+    // printSolution(n, matrix);
 
     inputFile.close();
 
